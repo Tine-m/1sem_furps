a88ac9463cc2b6abc9ba85a22a6a1f708dcfb97a
Tine-m <tinemarbjerg@gmail.com>
2022-04-26 12:08:33 +0200

    check git works

--- src/robustness/Main.java
+++ src/robustness/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     public static void main(String[] args) throws ParseException {
         UI ui = new UI();
+        System.out.println("hey");
         System.out.println("Echo of input: " + ui.readInteger());
         System.out.println("Echo of input: " + ui.readIntegerImproved());
         System.out.println("Echo of input: " + ui.readDouble());
